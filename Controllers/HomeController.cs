5e7b69c14916d52e5447e1ddc5b9cf542b008667
Dave Ward <dave@encosia.com>
2014-03-17 11:26:08 -0400

    Initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers {
+  public class HomeController : Controller {
+    // GET: Index
+    public ActionResult Index() {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Index(IndexViewModel Model) {
+      // Depending on your preference, you can either set the user agent here,
+      //  or inside the model class itself (see IndexModels.cs).
+      Model.UserAgent = Request.UserAgent;
+
+      Model.SendEmail();
+
+      return View(Model);
+    }
+  }
+}
---
+++ Models/IndexModels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace WebApplication4.Models {
+  public class IndexViewModel {
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+    public string UserAgent { get; set; }
+
+    public void SendEmail() {
+      // If you don't set the user agent in the controller action (see IndexController.cs), you
+      //  could also use HttpContext.Current to set it somewhere without access to the convenience
+      //  properties on the Controller object, like here:
+      UserAgent = HttpContext.Current.Request.UserAgent;
+
+      var message = new MailMessage();
+
+      message.From = new MailAddress(Email, Name);
+
+      message.To.Add("dave@encosia.com");
+
+      message.Subject = "Contact form from test site.";
+
+      message.Body = String.Format("Message: {0}\n\nUser-agent: {1}", Message, UserAgent);
+
+      var smtpClient = new SmtpClient();
+
+      // For testing, put emails in a local directory instead of trying to send them via SMTP.
+      smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+      smtpClient.PickupDirectoryLocation = HttpContext.Current.Server.MapPath("~/SentMessages");
+
+      smtpClient.Send(message);
+    }
+  }
+}
